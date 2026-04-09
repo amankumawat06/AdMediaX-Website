9d4d5ecc94bb2349bf8c901d693938ff70a8fcaf
Aman kumawat <amankumawat4507@gmail.com>
2026-04-09 13:17:53 +0530

    give access to backend to run ai agent url

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5501"]
+    origin: ["http://127.0.0.1:5501","https://amankumawat06.github.io"]
   }),
 );
 
